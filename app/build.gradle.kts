2207e158143398695fb55006fe2c97646b938dad
Jimmy Wu <13085770@qq.com>
2026-05-07 21:40:02 +0800

    fix: 器具下拉列表添加offset(y=4dp)，修正垂直位置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.coffeelab.coffeenotes"
         minSdk = 30
         targetSdk = 34
-        versionCode = 19
-        versionName = "1.2.7"
+        versionCode = 20
+        versionName = "1.2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
