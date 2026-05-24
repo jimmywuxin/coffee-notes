37cdd6f0998ffa4df379c2f72308ca8695c0e947
Jimmy Wu <13085770@qq.com>
2026-05-24 10:32:33 +0800

    v1.8.1: 修复备份导入器具/磨豆机重复插入

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.coffeelab.coffeenotes"
         minSdk = 30
         targetSdk = 34
-        versionCode = 53
-        versionName = "1.8.0"
+        versionCode = 54
+        versionName = "1.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
